fc3dc27749da3b05dc2bc5670f478d23eb6f44af
kobi070 <kobi070@gmail.com>
2024-07-16 11:15:42 +0300

    #update: updated version of layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,21 +7,23 @@ export const metadata = {
     title: 'Promptopia',
     description: 'Discover & Share AI Prompts'
 };
-const RootLayout = ({children, ...props}) => {
-  return (
-    <html lang="en">
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
+const RootLayout = ({ children, ...props }) => {
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
 
-            <main className='app'>
-            <Nav />
-                {children}
-            </main>
-        </body>
-    </html>
-  )
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
