08e7cb1298751d83e6088c7a176f5f3c3251d045
kobi070 <kobi070@gmail.com>
2024-07-18 09:04:24 +0300

    update: updated version of comoponent Profile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import PromptCard from './PromptCard'
+import PromptCard from "./PromptCard";
 
-const ProfileComponent = ({
-  name,
-  desc,
-  data,
-  handleEdit,
-  handleDelete
-}) => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
-      <h1>
-        {name} Profile
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{name} Profile</span>
       </h1>
-    </section>
-  )
-}
+      <p className='desc text-left'>{desc}</p>
 
-export default ProfileComponent
+      <div className='mt-10 prompt_layout'>
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
